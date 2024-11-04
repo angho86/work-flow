9bb4b3bc1c200034b6bbe530cc26af4a1ec9abe0
angho86 <myboks123@gmail.com>
2024-11-04 17:26:21 +0200

    start creating milestone class

---
+++ Assets/js/milestones.js
@@ -0,0 +1,14 @@
+class milestone{
+    constructor(name) {
+        this.name = name;
+        this.status = "N";
+    }
+}
+
+class milestones {
+    constructor(){
+        this.milestones = [];
+        this.mileElement = document.getElementById("milestone-list");
+        const addMilestoneBtn = document.getElementById("milestone-add");
+    }
+}
